1b877bb940210a5d5eacc17a72a927d28a8bfc73
aowis-becker <github@aowis.org>
2026-06-09 19:08:41 +0200

    adding favicon

--- compile_wasm.sh
+++ compile_wasm.sh
@@ -10,6 +10,7 @@ docker run --rm \
     mattbas/qt-emscripten:6.10.2 \
     /bin/bash /project/tools/qt-emscripten/build_wasm_inside_container.sh
 
-ln -sf aowis-epanet-gui.html build-wasm/index.html
+ln -sf aowis-server-gui.html build-wasm/index.html
+cp assets/img/favicon.ico build-wasm/.
 
 echo "=== WASM build finished ==="
