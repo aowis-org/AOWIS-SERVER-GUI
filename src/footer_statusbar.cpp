9f193933ce5cd1cb43a12346c14b16c34b907d21
aowis-becker <github@aowis.org>
2026-06-19 00:17:29 +0200

    WGS84 with 6 decimals

--- src/footer_statusbar.cpp
+++ src/footer_statusbar.cpp
@@ -43,8 +43,13 @@ void FooterStatusBar::setMapZoom(int zoom)
 }
 void FooterStatusBar::setMapCoordinatesWGS84(double lon, double lat)
 {
-    this->label_map_coords_lat->setText("WGS84 (GPS) Lat: " + QString::number(lat));
-    this->label_map_coords_lon->setText("Lon: " + QString::number(lon));
+    this->label_map_coords_lat->setText(
+        "WGS84 (GPS) Lat: " + QString::number(lat, 'f', 6)
+        );
+    
+    this->label_map_coords_lon->setText(
+        "Lon: " + QString::number(lon, 'f', 6)
+        );
 }
 void FooterStatusBar::setMapCoordinatesCRS(double lon, double lat)
 {
