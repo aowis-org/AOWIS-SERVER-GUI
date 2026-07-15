3753c0fd0e091b3a2adffcfc53fd4ad78aa6b134
aowis-becker <github@aowis.org>
2026-07-15 20:56:02 +0200

    db test successful

--- src/data.cpp
+++ src/data.cpp
@@ -4,10 +4,10 @@ Data::Data(QObject *parent)
     : QObject{parent},
     database_gui(new DatabaseGui(this))
 {
-    
+    connect(this->database_gui, &DatabaseGui::signalReady, this, &Data::getTest);
 }
 
 void Data::getTest()
 {
-    qDebug() << "FROM SQLITE: " << this->database_gui->getTestName();
+    qDebug() << "FROM SQLITE:" << this->database_gui->getTestName();
 }
