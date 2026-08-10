235ffffaead9cfb38a9386c8578ffe90bf4ae41d
aowis-becker <github@aowis.org>
2026-08-10 15:58:42 +0200

    one network snapshot builder

--- src/hydraulic_data.cpp
+++ src/hydraulic_data.cpp
@@ -1,4 +1,5 @@
 #include "hydraulic_data.h"
+#include "network_render_snapshot_builder.h"
 
 #include <cmath>
 
@@ -33,40 +34,6 @@ void updateMinimumMaximum(double value, double &minimum, double &maximum, bool &
         maximum = value;
 }
 
-template <typename LinkType>
-bool appendNetworkRenderLink(const LinkType &source, InfrastructureEntity entity_type, quint32 render_id,
-                             const QList<NetworkRenderNode> &nodes,
-                             const QHash<QUuid, qsizetype> &node_indices,
-                             QList<NetworkRenderLink> &links)
-{
-    const QHash<QUuid, qsizetype>::const_iterator start_iterator =
-        node_indices.constFind(source.node_uuid_from);
-    const QHash<QUuid, qsizetype>::const_iterator end_iterator =
-        node_indices.constFind(source.node_uuid_to);
-
-    if (start_iterator == node_indices.cend() || end_iterator == node_indices.cend())
-        return false;
-
-    const NetworkRenderNode &start_node = nodes.at(start_iterator.value());
-    const NetworkRenderNode &end_node = nodes.at(end_iterator.value());
-
-    NetworkRenderLink link;
-    link.render_id = render_id;
-    link.id = source.id;
-    link.uuid = source.uuid;
-    link.entity_type = entity_type;
-    link.start_node_render_id = start_node.render_id;
-    link.end_node_render_id = end_node.render_id;
-    link.vertices_wgs84.reserve(source.vertices.size() + 2);
-    link.vertices_wgs84.append(start_node.coordinate_wgs84);
-
-    for (const HydraulicLinkVertex &vertex : source.vertices)
-        link.vertices_wgs84.append(vertex.coordinate_wgs84);
-
-    link.vertices_wgs84.append(end_node.coordinate_wgs84);
-    links.append(link);
-    return true;
-}
 }
 
 HydraulicData::HydraulicData(QObject *parent)
@@ -1473,83 +1440,8 @@ void HydraulicData::rebuildBoundingBoxWgs84()
 
 void HydraulicData::rebuildNetworkRenderSnapshot() const
 {
-    NetworkRenderSnapshot snapshot;
-    snapshot.geometry_revision = this->geometry_revision;
-    snapshot.visual_revision = this->visual_revision;
-
-    const qsizetype node_count = this->network_hydraulic.nodes_junctions.size() +
-                                 this->network_hydraulic.nodes_reservoirs.size() +
-                                 this->network_hydraulic.nodes_tanks.size();
-    const qsizetype link_count = this->network_hydraulic.links_pipes.size() +
-                                 this->network_hydraulic.links_pumps.size() +
-                                 this->network_hydraulic.links_valves.size();
-    snapshot.nodes.reserve(node_count);
-    snapshot.links.reserve(link_count);
-
-    QHash<QUuid, qsizetype> node_indices;
-    node_indices.reserve(node_count);
-    quint32 next_node_render_id = 1;
-
-    for (const HydraulicNodeJunction &source : this->network_hydraulic.nodes_junctions)
-    {
-        NetworkRenderNode node;
-        node.render_id = next_node_render_id++;
-        node.id = source.id;
-        node.uuid = source.uuid;
-        node.entity_type = InfrastructureEntity::Junction;
-        node.coordinate_wgs84 = source.coordinate_wgs84;
-        node_indices.insert(node.uuid, snapshot.nodes.size());
-        snapshot.nodes.append(node);
-    }
-
-    for (const HydraulicNodeReservoir &source : this->network_hydraulic.nodes_reservoirs)
-    {
-        NetworkRenderNode node;
-        node.render_id = next_node_render_id++;
-        node.id = source.id;
-        node.uuid = source.uuid;
-        node.entity_type = InfrastructureEntity::Reservoir;
-        node.coordinate_wgs84 = source.coordinate_wgs84;
-        node_indices.insert(node.uuid, snapshot.nodes.size());
-        snapshot.nodes.append(node);
-    }
-
-    for (const HydraulicNodeTank &source : this->network_hydraulic.nodes_tanks)
-    {
-        NetworkRenderNode node;
-        node.render_id = next_node_render_id++;
-        node.id = source.id;
-        node.uuid = source.uuid;
-        node.entity_type = InfrastructureEntity::Tank;
-        node.coordinate_wgs84 = source.coordinate_wgs84;
-        node_indices.insert(node.uuid, snapshot.nodes.size());
-        snapshot.nodes.append(node);
-    }
-
-    quint32 next_link_render_id = 1;
-
-    for (const HydraulicLinkPipe &source : this->network_hydraulic.links_pipes)
-    {
-        if (appendNetworkRenderLink(source, InfrastructureEntity::Pipe, next_link_render_id,
-                                    snapshot.nodes, node_indices, snapshot.links))
-            ++next_link_render_id;
-    }
-
-    for (const HydraulicLinkPump &source : this->network_hydraulic.links_pumps)
-    {
-        if (appendNetworkRenderLink(source, InfrastructureEntity::Pump, next_link_render_id,
-                                    snapshot.nodes, node_indices, snapshot.links))
-            ++next_link_render_id;
-    }
-
-    for (const HydraulicLinkValve &source : this->network_hydraulic.links_valves)
-    {
-        if (appendNetworkRenderLink(source, InfrastructureEntity::Valve, next_link_render_id,
-                                    snapshot.nodes, node_indices, snapshot.links))
-            ++next_link_render_id;
-    }
-
-    this->network_render_snapshot = snapshot;
+    this->network_render_snapshot = buildNetworkRenderSnapshot(
+        this->network_hydraulic, this->geometry_revision, this->visual_revision);
 }
 
 void HydraulicData::setSelectedUuid(InfrastructureEntity entity_type, const QUuid &uuid)
